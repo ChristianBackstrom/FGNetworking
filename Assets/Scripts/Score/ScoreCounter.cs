714fc7003d83d264b6ae9a479095e38bfa6525da
ChristianBackstrom <christian.berggren.backstrom@gmail.com>
2024-03-12 17:59:15 +0100

    added events for changes in scoreboard

--- Assets/Scripts/Score/ScoreCounter.cs
+++ Assets/Scripts/Score/ScoreCounter.cs
@@ -3,17 +3,22 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
+using Object = System.Object;
 
 public static class ScoreCounter
 {
     private static Dictionary<ulong, int> scoreboard;
-
     public static Dictionary<ulong, int> Scoreboard => scoreboard;
+    public static event EventHandler<ScoreData> OnValueChanged;
+    public static event EventHandler<ulong> ClientDisconnected;
+    public static event EventHandler<ulong> ClientConnected;
+
 
     public static void AddClient(ulong networkID)
     {
         if (scoreboard == null) scoreboard = new Dictionary<ulong, int>();
         scoreboard.Add(networkID, 0);
+        ClientConnected?.Invoke(typeof(ScoreCounter), networkID);
     }
     
     public static void RemoveClient(ulong networkID)
@@ -21,6 +26,7 @@ public static class ScoreCounter
         try
         {
             scoreboard.Remove(networkID);
+            ClientDisconnected?.Invoke(typeof(ScoreCounter), networkID);
         }
         catch (Exception e)
         {
@@ -32,10 +38,31 @@ public static class ScoreCounter
     public static void IncreaseScore(ulong networkID)
     {
         scoreboard[networkID]++;
+        OnValueChanged?.Invoke(typeof(ScoreCounter), new ScoreData()
+        {
+            NetworkID = networkID,
+            Score = scoreboard[networkID],
+        });
+    }
+    
+    public static void DecreaseScore(ulong networkID)
+    {
+        scoreboard[networkID]--;
+        OnValueChanged?.Invoke(typeof(ScoreCounter), new ScoreData()
+        {
+            NetworkID = networkID,
+            Score = scoreboard[networkID],
+        });
     }
 
     public static int GetScore(ulong networkID)
     {
         return scoreboard[networkID];
     }
+}
+
+public struct ScoreData
+{
+    public ulong NetworkID;
+    public int Score;
 }
